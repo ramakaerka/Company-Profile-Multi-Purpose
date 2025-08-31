43419b1da041beb38e9d609a30c0c083803c9d96
ramakaerka <ramakaerka@gmail.com>
2025-09-01 01:44:45 +0700

    Initial commit

---
+++ app/Models/Appointment.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Appointment extends Model
+{
+    use HasFactory, SoftDeletes;
+    protected $fillable = [
+        'name',
+        'phone_number',
+        'email',
+        'budget',
+        'brief',
+        'meeting_at',
+        'product_id',
+    ];
+
+    protected $casts = [
+        'meeting_at' => 'date',
+    ];
+
+    public function product(){
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+}
